ce4847c9722a8e1102e4525bc828d0faa29abed7
MBS20350 <79281100+MBS20350@users.noreply.github.com>
2021-04-25 19:52:52 -0400

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -22,6 +22,10 @@ data <- data %>%
 dev.new(width=480, height=480, unit="px") 
 plot(data$Time2, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 points(data$Time2, data$Sub_metering_2, type = "l", col = "red")
+points(data$Time2, data$Sub_metering_3, type = "l", col = "blue")
+series_names <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+legend("topright", lty=1, col = c("black", "red", "blue"), legend = series_names, text.width = strwidth(series_names)[1]*1.6)
+
 
 ## Copy to PNG file
 
