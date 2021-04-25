d15cf118d7b2d15e180ae95fe0d4443ebafe18a0
MBS20350 <79281100+MBS20350@users.noreply.github.com>
2021-04-25 11:58:16 -0400

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -3,20 +3,25 @@
 ## Mark Sucato
 
 library(dplyr)
-library(lubridate)
 
-## Load data and convert date/time variables to date/time classes via lubridate
+## Load data
+## Convert date/time variables to date/time classes
+## Limit data to two-day period in Feb 2007
 
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 data <- data %>%
-	mutate(Date = dmy(Date)) %>%
-	mutate(Time = hms(Time))
+  mutate(Date = as.Date(Date, "%d/%m/%Y")) %>%
+  mutate(Time = hms::as_hms(strptime(Time, "%T"))) %>%
+  filter((Date >= as.Date("2007-02-01")) & (Date <= as.Date("2007-02-02")))
 
-## Need to select just data for 2007-02-01 and 2007-02-02
+## Change dimensions to 480x480 px
+## Create Plot 1 
 
-
-
-
-
+dev.new(width=480, height=480, unit="px")  
 hist(data$Global_active_power, col = "red", xlab = NULL, main = NULL)
 title(main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+## Copy to PNG file
+
+dev.copy(png, file = "plot1.png")
+dev.off()
